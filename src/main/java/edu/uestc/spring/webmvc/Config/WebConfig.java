22466ea82b1d3736b0f7a8333d3f974e7b00525f
timelessfb <timelessfb>
2017-12-06 00:46:59 +0800

    home update

--- src/main/java/edu/uestc/spring/webmvc/Config/WebConfig.java
+++ src/main/java/edu/uestc/spring/webmvc/Config/WebConfig.java
@@ -1,10 +1,12 @@
 package edu.uestc.spring.webmvc.Config;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
-@ComponentScan(basePackages = "edu.uestc.spring.webmvc")
-public class RootConfig {
+@Configurable
+@EnableWebMvc
+@ComponentScan(basePackages = "")
+public class WebConfig {
+
 }
--- src/main/java/edu/uestc/spring/webmvc/SpittrWebAppInitializer.java
+++ src/main/java/edu/uestc/spring/webmvc/SpittrWebAppInitializer.java
@@ -1,6 +1,7 @@
 package edu.uestc.spring.webmvc;
 
 import edu.uestc.spring.webmvc.Config.RootConfig;
+import edu.uestc.spring.webmvc.Config.WebConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -8,15 +9,15 @@ public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherS
     /*getRootConfigClasses()方法返回的带有@Configuration注解的类将会用来配置ContextLoaderListener创建的Spring application context中的bean*/
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?> {
-            RootConfig.class
-        } ;
+        return new Class<?>[]{
+                RootConfig.class
+        };
     }
 
     /*返回的带有@Configuration注解的类，将会用来定义DispatcherServlet的Spring application context中的bean*/
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class<?>[]{WebConfig.class};
     }
 
     /*将一个或多个路径映射到DispatcherServlet*/
