b61459f18f3fc1d6cfd4303ddc60e869987225c6
timelessfb <1282374749@qq.com>
2017-12-05 22:43:33 +0800

    update3

--- src/main/java/edu/uestc/spring/webmvc/SpittrWebAppInitializer.java
+++ src/main/java/edu/uestc/spring/webmvc/SpittrWebAppInitializer.java
@@ -1,10 +1,10 @@
 package edu.uestc.spring.webmvc;
 
-import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
 
+    /*getRootConfigClasses()方法返回的带有@Configuration注解的类将会用来配置ContextLoaderListener创建的Spring application context中的bean*/
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[0]{RootConfig.class};
@@ -16,7 +16,7 @@ public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherS
         return new Class[0];
     }
 
-    /*一个或多个路径映射到DispatcherServlet*/
+    /*将一个或多个路径映射到DispatcherServlet*/
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
